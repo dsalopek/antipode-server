c08a2da357237f02074611f89bbfdd0f444cf2e3
Dylan Salopek <dylan.salopek@gmail.com>
2020-09-08 16:20:31 -0500

    Add param logging support to loggable methods

--- src/main/java/io/salopek/logging/Loggable.java
+++ src/main/java/io/salopek/logging/Loggable.java
@@ -8,4 +8,5 @@ import java.lang.annotation.Target;
 @Target(value = {ElementType.METHOD, ElementType.TYPE})
 @Retention(value = RetentionPolicy.RUNTIME)
 public @interface Loggable {
+  boolean params() default false;
 }
--- src/main/java/io/salopek/logging/LoggingAspect.java
+++ src/main/java/io/salopek/logging/LoggingAspect.java
@@ -4,8 +4,11 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Method;
 
 @Aspect
 public class LoggingAspect {
@@ -18,14 +21,29 @@ public class LoggingAspect {
   public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
     final Logger logger = LoggerFactory.getLogger(joinPoint.getSignature().getDeclaringType());
     long start = System.currentTimeMillis();
-    String method = joinPoint.getSignature().toShortString();
-    LogBuilder lb = LogBuilder.get().log(LogUtils.methodEntry(method));
+
+    MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+    Method method = signature.getMethod();
+
+    String methodName = joinPoint.getSignature().getName();
+    LogBuilder lb = LogBuilder.get().log(LogUtils.methodEntry(methodName));
+
+    Loggable loggable = method.getAnnotation(Loggable.class);
+
+    if(loggable.params()) {
+      String[] paramNames = signature.getParameterNames();
+      Object[] paramValues = joinPoint.getArgs();
+      for(int i = 0; i < paramNames.length; i++) {
+        lb.kv(paramNames[i], paramValues[i].toString());
+      }
+    }
+
     logger.info(lb.build());
 
     Object result = joinPoint.proceed();
     long duration = System.currentTimeMillis() - start;
 
-    lb.log(LogUtils.methodExit(method, duration));
+    lb.log(LogUtils.methodExit(methodName, duration));
     logger.info(lb.build());
     return result;
   }
